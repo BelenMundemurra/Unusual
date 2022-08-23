947fb46c705a8569acea5a498cd6f93894b87133
Belen Munde <munde.belen@gmail.com>
2022-08-22 21:15:59 -0300

    Menú e-commerce de ropa

--- my-app/src/components/App.jsx
+++ my-app/src/components/App.jsx
@@ -5,28 +5,28 @@ function App() {
     <div className="App">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Navbar</a>
+          <a className="navbar-brand" href="#">SUNDAE</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
           </button>
           <div className="collapse navbar-collapse" id="navbarColor03">
             <ul className="navbar-nav me-auto">
               <li className="nav-item">
-                <a className="nav-link active" href="#">Home
+                <a className="nav-link active" href="#">Inicio
                   <span className="visually-hidden">(current)</span>
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">About</a>
+                <a className="nav-link" href="#">Sobre Nosotros</a>
               </li>
               <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Tienda</a>
                 <div className="dropdown-menu">
-                  <a className="dropdown-item" href="#">Action</a>
-                  <a className="dropdown-item" href="#">Another action</a>
-                  <a className="dropdown-item" href="#">Something else here</a>
+                  <a className="dropdown-item" href="#">Remeras</a>
+                  <a className="dropdown-item" href="#">Polleras</a>
+                  <a className="dropdown-item" href="#">Buzos</a>
                   <div className="dropdown-divider" />
-                  <a className="dropdown-item" href="#">Separated link</a>
+                  <a className="dropdown-item" href="#">Accesorios</a>
                 </div>
               </li>
             </ul>
